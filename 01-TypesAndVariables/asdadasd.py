96f3d71f8c8534102f8d24bbf048e80e2e25010c
TraceOnBrainOff <40839856+TraceOnBrainOff@users.noreply.github.com>
2019-10-14 14:51:21 +0200

    Add files via upload

---
+++ 01-TypesAndVariables/asdadasd.py
@@ -0,0 +1,4 @@
+#Zapisywanie zmiennych z shella i cd str.format()
+imie = input("Podaj imie: ")
+nazwisko = input("Podaj nazwisko: ")
+print(str.format("Imie to {}, a nazwisko to {}", imie, nazwisko))
